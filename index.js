4e275e3da66dc86099dc598af7910ae5855cacfe
AliMukhtar99 <ali.mukhta@student.moringaschool.com>
2022-08-17 16:04:09 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = (parameter1, parameter2) => {
+    return 2000/ 100;
+}
+
+const square = (parameter1, parameter2) => {
+    return 2* 2;
+}
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(3,4);
